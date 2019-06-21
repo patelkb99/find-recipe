85085a3f10979a9c75029033372da3daf1e0c1f9
Krishna Patel <krishna.patel@honeywell.com>
2019-06-21 16:52:25 -0400

    work on the react app

--- find-recipe-api/dist/recipes/recipes.controller.js
+++ find-recipe-api/dist/recipes/recipes.controller.js
@@ -16,8 +16,6 @@ const common_1 = require("@nestjs/common");
 const rxjs_1 = require("rxjs");
 const recipes_service_1 = require("./recipes.service");
 const operators_1 = require("rxjs/operators");
-const config_1 = require("../config");
-const recipes_util_1 = require("./recipes.util");
 let RecipesController = class RecipesController {
     constructor(recipesService) {
         this.recipesService = recipesService;
@@ -26,17 +24,84 @@ let RecipesController = class RecipesController {
         return this.recipesService.getRecipePuppyData(`http://www.recipepuppy.com/api/?i=${i}&q=${q}`).pipe(operators_1.map(response => response.data.results));
     }
     getRandomRecipe(i, q) {
-        return this.recipesService.getRecipePuppyData(`http://www.recipepuppy.com/api/?i=${i}&q=${q}`).pipe(recipes_util_1.findRandomRecipe(), recipes_util_1.makeRandomFullRecipe(), operators_1.flatMap((recipe) => {
-            var headers = {
-                'x-app-id': `${config_1.default.NUTRITION_APP_ID}`,
-                'x-app-key': `${config_1.default.NUTRITION_APP_KEY}`,
-                'x-remote-user-id': `${config_1.default.NUTRITION_REMOTE_USER_ID}`
-            };
-            var payload = {
-                'query': recipe.ingredients
-            };
-            return this.recipesService.getDownstreamFoodsResponseData('https://trackapi.nutritionix.com/v2/natural/nutrients', payload, headers).pipe(recipes_util_1.reduceDownstreamNutritionResponse(recipe));
-        }));
+        return {
+            title: "Easy Stracciatella Italian Soup",
+            href: "http://www.recipezaar.com/Easy-Stracciatella-Italian-Soup-275229",
+            ingredients: "cheese, chicken broth, eggs, spinach, pasta, salt",
+            thumbnail: "http://img.recipepuppy.com/529552.jpg",
+            nutrition: [
+                {
+                    food: "cheese",
+                    serving_qty: 1,
+                    calories: 113.12,
+                    total_fat: 9.33,
+                    saturated_fat: 5.28,
+                    cholesterol: 27.72,
+                    sodium: 182.84,
+                    carbohydrate: 0.87,
+                    fiber: 0,
+                    sugars: 0.13,
+                    protein: 6.4,
+                    potassium: 21.28
+                },
+                {
+                    food: "chicken broth",
+                    serving_qty: 1,
+                    calories: 14.94,
+                    total_fat: 0.52,
+                    saturated_fat: 0.03,
+                    cholesterol: 4.98,
+                    sodium: 923.79,
+                    carbohydrate: 1.1,
+                    fiber: 0,
+                    sugars: 1.07,
+                    protein: 1.59,
+                    potassium: 44.82
+                },
+                {
+                    food: "eggs",
+                    serving_qty: 1,
+                    calories: 71.5,
+                    total_fat: 4.76,
+                    saturated_fat: 1.56,
+                    cholesterol: 186,
+                    sodium: 71,
+                    carbohydrate: 0.36,
+                    fiber: 0,
+                    sugars: 0.19,
+                    protein: 6.28,
+                    potassium: 69
+                },
+                {
+                    food: "spinach pasta",
+                    serving_qty: 1,
+                    calories: 175.5,
+                    total_fat: 1.27,
+                    saturated_fat: 0.29,
+                    cholesterol: 44.55,
+                    sodium: 8.1,
+                    carbohydrate: 33.8,
+                    fiber: null,
+                    sugars: null,
+                    protein: 6.83,
+                    potassium: 49.95
+                },
+                {
+                    food: "salt",
+                    serving_qty: 0.25,
+                    calories: 0,
+                    total_fat: 0,
+                    saturated_fat: 0,
+                    cholesterol: 0,
+                    sodium: 581.37,
+                    carbohydrate: 0,
+                    fiber: 0,
+                    sugars: 0,
+                    protein: 0,
+                    potassium: 0.12
+                }
+            ]
+        };
     }
 };
 __decorate([
@@ -51,7 +116,7 @@ __decorate([
     __param(0, common_1.Query('i')), __param(1, common_1.Query('q')),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [String, String]),
-    __metadata("design:returntype", rxjs_1.Observable)
+    __metadata("design:returntype", Object)
 ], RecipesController.prototype, "getRandomRecipe", null);
 RecipesController = __decorate([
     common_1.Controller('recipes'),
