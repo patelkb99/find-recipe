b7d17e4c71a036a3dab16435045d13a559f693cb
Krishna Patel <krishna.patel@honeywell.com>
2019-06-25 16:19:27 -0400

    formatting and pics

--- find-recipe-react/src/App.js
+++ find-recipe-react/src/App.js
@@ -5,10 +5,19 @@ import Input from './Input';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to Recipe Finder!
-      </header>
+      <div className="App-header">
+        <header >
+          <div className="header">
+            <h4>Welcome to Recipe Finder! </h4>
+            <img src="logo_clear.png" alt="Find Recipe Logo" height="15%"/>
+          </div>
+        </header>
+      </div>
+      
       <body className="App-body">
+        <div className="description"> 
+          <p>Pick a cuisine (or leave it as any) and up to 3 ingredients to cook with.<br /> Recipe Finder will find you something to cook!</p>
+        </div>
         <Input></Input>
         </body>
     </div>
--- find-recipe-react/src/Input.js
+++ find-recipe-react/src/Input.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Input.css';
-// import Recipe from './App';
 
 class Input extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        found: false,
         recipe: {},
         cuisine: "Any",
         ingredient1: "",
         ingredient2: "",
-        ingredient3: ""
+        ingredient3: "",
       };
   
       this.onIngredient1Change = this.onIngredient1Change.bind(this);
@@ -43,25 +43,27 @@ class Input extends React.Component {
             cuisine: event.target.value
         });
     }
-  
+
     async handleSubmit(event) {
         event.preventDefault();
         console.log(this.state.ingredient1);
         console.log(this.state.cuisine);
 
-
         try {
+            console.log(`http://localhost:3000/recipes/random/?i=${this.state.ingredient1},${this.state.ingredient2},${this.state.ingredient3}&q=${this.state.cuisine}`)
             await fetch(`http://localhost:3000/recipes/random/?i=${this.state.ingredient1},${this.state.ingredient2},${this.state.ingredient3}&q=${this.state.cuisine}`)
             .then(response => response.json())
             .then(
                 (result) => {
                     this.setState({
-                        recipe: result
+                        recipe: result,
+                        found: true
                     });
             }).catch((error) => { 
                 console.log(error); 
             });
-            alert("Cuisine: " + this.state.recipe.title + " + Ingredients: " + this.state.recipe.ingredients );
+            document.getElementById('recipe-response').style.display = "block";
+
         } catch (error) {
             console.log(error);
         }
@@ -71,48 +73,78 @@ class Input extends React.Component {
             ingredient2: "",
             ingredient3: "",
         });
-    }
+    }  
 
     render() {
+        const found = this.state.found;
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Cuisine* :
-                    <select value={this.state.cuisine} onChange={this.onSelectChange} required>
-                        <option defaultValue="Any">Any</option>
-                        <option value="Asian">Asian</option>
-                        <option value="Italian">Italian</option>
-                        <option value="Thai">Thai</option>
-                    </select>
-                </label>
-                <br></br>
-                <label>
-                    Ingredient 1:
-                    <input
-                        name="Ingredient 1"
-                        type="text"
-                        value={this.state.ingredient1}
-                        onChange={this.onIngredient1Change} />
+            <div className="input">
+                <div className="form">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <div className="custom-select">
+                                Cuisine* :
+                                <select value={this.state.cuisine} onChange={this.onSelectChange}>
+                                    <option value="">Any Cuisine</option>                                    
+                                    <option value="American">American</option>
+                                    <option value="Chinese">Chinese</option>
+                                    <option value="Colombian">Colombian</option>
+                                    <option value="French">French</option>
+                                    <option value="Greek">Greek</option>
+                                    <option value="Indian">Indian</option>
+                                    <option value="Italian">Italian</option>
+                                    <option value="Japanese">Japanese</option>
+                                    <option value="Korean">Korean</option>
+                                    <option value="Lebanese">Lebanese</option>
+                                    <option value="Mediterranean">Mediterranean</option>
+                                    <option value="Mexican">Mexican</option>
+                                    <option value="Spanish">Spanish</option>
+                                    <option value="Caribbean">Italian</option>
+                                    <option value="Thai">Thai</option>
+                                </select>
+                            </div>
+                        </label>
+                        <br></br>
+                        <label>
+                            Ingredient 1:
+                            <input
+                                name="Ingredient 1"
+                                type="text"
+                                value={this.state.ingredient1}
+                                onChange={this.onIngredient1Change} />
+                            <br></br>
+                            Ingredient 2:
+                            <input
+                                name="Ingredient 2"
+                                type="text"
+                                value={this.state.ingredient2}
+                                onChange={this.onIngredient2Change} />
+                            <br></br>
+                            Ingredient 3:
+                            <input
+                                name="Ingredient 3"
+                                type="text"
+                                value={this.state.ingredient3}
+                                onChange={this.onIngredient3Change} />
+                        </label>
+                        <br></br>
+                        <div className="submit">
+                            <input name="submit-button" type="submit" value="Submit"/>
+                        </div>
+                            
+                    </form>
+                </div>
+                <div id="recipe-response" style={{display: this.state.found ? 'block' : 'none' }} >
+                    <h4>You should cook <a href={this.state.recipe.href} target="_blank">{this.state.recipe.title}</a> today!</h4>
                     <br></br>
-                    Ingredient 2:
-                    <input
-                        name="Ingredient 2"
-                        type="text"
-                        value={this.state.ingredient2}
-                        onChange={this.onIngredient2Change} />
                     <br></br>
-                    Ingredient 3:
-                    <input
-                        name="Ingredient 3"
-                        type="text"
-                        value={this.state.ingredient3}
-                        onChange={this.onIngredient3Change} />
-                </label>
-                <br></br>
-                <input type="submit" value="Submit" />
-            </form>
+                    <br></br>
+                    <p>Ingredients: {this.state.recipe.ingredients}</p>
+                </div>
+            </div>
         );
+        
     }
-  }
+}
 
-  export default Input;
+export default Input;
