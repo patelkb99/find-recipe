eb13551fb1e4afbcc4dc4ed38570fc0a1a1fffe5
Krishna Patel <krishna.patel@honeywell.com>
2019-06-25 09:27:32 -0400

    fix fetch in front end; sucessfully pulls from database

--- find-recipe-react/src/App.js
+++ find-recipe-react/src/App.js
@@ -10,67 +10,9 @@ function App() {
       </header>
       <body className="App-body">
         <Input></Input>
-        <Recipe></Recipe>
         </body>
     </div>
   );
-}
-
-class Recipe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      recipe: {}
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:3000/recipes/random/?i=eggs,cheese&q=italian")
-      .then(response => {
-        return response.json();
-      }).then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            recipe: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, recipe } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-            <li key={recipe.title}>
-              Recipe Name: {recipe.title}
-            </li>
-            <li key={recipe.href}>
-              {recipe.href}
-            </li>
-            <li key={recipe.ingredients}>
-              {recipe.ingredients}
-            </li>
-        </ul>
-      );
-    }
-  }
 }
 
 export default App;
--- find-recipe-react/src/Input.js
+++ find-recipe-react/src/Input.js
@@ -1,40 +1,85 @@
 import React from 'react';
 import './Input.css';
+// import Recipe from './App';
 
 class Input extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        recipe: {},
         cuisine: "Any",
         ingredient1: "",
         ingredient2: "",
         ingredient3: ""
       };
   
+      this.onIngredient1Change = this.onIngredient1Change.bind(this);
+      this.onIngredient2Change = this.onIngredient2Change.bind(this);
+      this.onIngredient3Change = this.onIngredient3Change.bind(this);
+      this.onSelectChange = this.onSelectChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
-      this.onInputChange = this.onInputChange.bind(this);
-
     }
 
-    onInputChange(event) {
-        this.setState({ingredient1: event.target.value});
-    }
-  
-    handleSubmit(event) {
+    onIngredient1Change(event) {
         this.setState({
-            cuisine: event.target.value,
             ingredient1: event.target.value
         });
-        alert("Cuisine: " + this.state.cuisine + " & Ingredient: " + this.state.ingredient1 );
+    }
+
+    onIngredient2Change(event) {
+        this.setState({
+            ingredient2: event.target.value
+        });
+    }
+
+    onIngredient3Change(event) {
+        this.setState({
+            ingredient3: event.target.value
+        });
+    }
+
+    onSelectChange(event) {
+        this.setState({
+            cuisine: event.target.value
+        });
     }
   
+    async handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.ingredient1);
+        console.log(this.state.cuisine);
+
+
+        try {
+            await fetch(`http://localhost:3000/recipes/random/?i=${this.state.ingredient1},${this.state.ingredient2},${this.state.ingredient3}&q=${this.state.cuisine}`)
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        recipe: result
+                    });
+            }).catch((error) => { 
+                console.log(error); 
+            });
+            alert("Cuisine: " + this.state.recipe.title + " + Ingredients: " + this.state.recipe.ingredients );
+        } catch (error) {
+            console.log(error);
+        }
+        this.setState({
+            cuisine: "Any",
+            ingredient1: "",
+            ingredient2: "",
+            ingredient3: "",
+        });
+    }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Is going:
-                    <select value={this.state.cuisine}>
-                        <option selected value="Any">Any</option>
+                    Cuisine* :
+                    <select value={this.state.cuisine} onChange={this.onSelectChange} required>
+                        <option defaultValue="Any">Any</option>
                         <option value="Asian">Asian</option>
                         <option value="Italian">Italian</option>
                         <option value="Thai">Thai</option>
@@ -42,12 +87,26 @@ class Input extends React.Component {
                 </label>
                 <br></br>
                 <label>
-                    Ingredient:
+                    Ingredient 1:
                     <input
                         name="Ingredient 1"
                         type="text"
                         value={this.state.ingredient1}
-                        onChange={this.onInputChange} />
+                        onChange={this.onIngredient1Change} />
+                    <br></br>
+                    Ingredient 2:
+                    <input
+                        name="Ingredient 2"
+                        type="text"
+                        value={this.state.ingredient2}
+                        onChange={this.onIngredient2Change} />
+                    <br></br>
+                    Ingredient 3:
+                    <input
+                        name="Ingredient 3"
+                        type="text"
+                        value={this.state.ingredient3}
+                        onChange={this.onIngredient3Change} />
                 </label>
                 <br></br>
                 <input type="submit" value="Submit" />
